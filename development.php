558132db21514f983c9607dc589da8d1c382df36
Nicklasos <nicklasos@gmail.com>
2016-05-30 12:09:30 +0300

    Fix

--- development.php
+++ development.php
@@ -2,7 +2,7 @@
 require 'WebhookProxy.php';
 
 define('SERVER_URL', 'https://dev-1.nibot.net/client.php');
-define('LOCAL_URL', 'http://localhost:3000/hello/telegram.php');
+define('LOCAL_URL', 'http://localhost:3000');
 define('DEVELOPER', 'nicklasos');
 
 $data = WebhookProxy\receive(SERVER_URL, $_GET['developer'] ?? DEVELOPER, isset($_GET['clear']));
